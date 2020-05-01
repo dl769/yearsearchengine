067714d3cba13bd45bda07125bfd281894516853
dl769 <qwertyx7xx@gmail.com>
2020-05-01 22:58:01 +0200

    build commit

---
+++ precache-manifest.4621c1ebb63a6568c80dba6d4db3db93.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294bfd5930b1b206b3dc541a5e5186bf",
+    "url": "/yearsearchengine/index.html"
+  },
+  {
+    "revision": "70073eaef59b6ede2a9b",
+    "url": "/yearsearchengine/static/css/main.3363789c.chunk.css"
+  },
+  {
+    "revision": "ba6b86a6491f211cbed2",
+    "url": "/yearsearchengine/static/js/2.0b928494.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/yearsearchengine/static/js/2.0b928494.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "70073eaef59b6ede2a9b",
+    "url": "/yearsearchengine/static/js/main.934f470e.chunk.js"
+  },
+  {
+    "revision": "1464341603c004f2ca73",
+    "url": "/yearsearchengine/static/js/runtime-main.ca184a9a.js"
+  },
+  {
+    "revision": "04be5c039734d18bf21360dab4602829",
+    "url": "/yearsearchengine/static/media/bbico.04be5c03.png"
+  },
+  {
+    "revision": "40ae7293704528cfce433d99be12b307",
+    "url": "/yearsearchengine/static/media/bg3.40ae7293.jpg"
+  }
+]);
